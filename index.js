169cfe37c59247c3fa49e3a3c782a4cd256463c7
ehojune <ehojune@gmail.com>
2021-07-24 13:05:16 +0900

    db id encrypted

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require("./config/key")
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ehojune:123321@cluster0.dj5wx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected. . .'))
 .catch(err => console.log(err))
 
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World~~~~~~~~!'))
+
+app.post('/register', (req, res) => {
+    //회원가입할때 필요한 정보들을 client에서 가져오면,
+    //데이터베이스에 넣어준다
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ sucess: false, err})
+        return res.status(200).json({ sucess: true})
+    })
+
+})
+
+
+
 app.listen(port, () => console.log(`example app listening on port ${port}!`))
